006f268a578ffcc3723b80a80a6edbdbd4c3c74a
William Wittenbrock <william.wittenbrock@gmail.com>
2019-01-24 14:13:59 -0800

    Add working translate button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './sass/main.scss';
 
-console.log("hi");
+// When the translate button is clicked:
+document.querySelector('.translate-button').addEventListener('click', () => {
+  const japanese = "人生のミッションは、世界一のラーメン屋さんを見つけること。";
+  const english = "This is an English translation.";
+  let visibleText = document.getElementById("visible-text");
+  
+  if (visibleText.innerHTML === japanese) {
+    visibleText.innerHTML = english;
+  } else {
+    visibleText.innerHTML = japanese;
+  }
+});
